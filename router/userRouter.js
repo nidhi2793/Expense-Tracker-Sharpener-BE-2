5cadea9f9c177c8f1164afc942d50ded701f81f6
nidhi2793 <MY_NAME@example.com>
2023-09-19 14:45:43 +0530

    Task 11 Leaderboard optimization

--- router/userRouter.js
+++ router/userRouter.js
@@ -9,6 +9,8 @@ router.get("/", userController.getLoginPage);
 
 router.get("/isPremiumUser", userAuthentication, userController.isPremiumUser);
 
+router.get("/getAllUsers", userController.getAllUsers);
+
 router.post("/login", userController.postUserLogin);
 
 router.post("/signUp", userController.postUserSignUp);
